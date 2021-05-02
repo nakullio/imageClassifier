7544791b2532b6b740f370a38a4b4a58787e1e06
Nakula <nakulamarvellio@gmail.com>
2021-05-02 21:47:43 +0700

    ADD: live camera prediction

--- index.js
+++ index.js
@@ -18,7 +18,22 @@ async function app() {
     // put webcame app
     const webcam = await tf.data.webcam(webcamElement);
 
-    console.log(result);
+    // console.log(result);
+    while(true) {
+
+        const img = await webcam.capture();
+        const result = await net.classify(img);
+
+        document.getElementById('console').innerText = `
+        prediction: ${result[0].className}\n
+        probability: ${result[0].probability}
+        `;
+
+    img.dispose();
+
+    await tf.nextFrame();
+
+    }
 }
 
 app();
